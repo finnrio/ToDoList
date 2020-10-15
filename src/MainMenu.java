dfda273335c87222226474b571bb7e5172d4c943
Archie Sanger-Davies <archiesd@outlook.com>
2020-10-15 15:36:46 +0100

    Added the functionality to add to the list using an ArrayList to store the list.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList; // import the ArrayList class
 
 public class MainMenu {
-    // method to print menu and get users choice
-    static String menu() {
-        // Scanner to get an input
-        Scanner input = new Scanner(System.in);
+
+    static String menu() { // method to print menu and get users choice
+        Scanner input = new Scanner(System.in); // Scanner to get an input
         String menu = ("==============================" +
                 "\nWelcome to the To Do list!" +
                 "\nWhat would you like to do?" +
@@ -17,12 +17,23 @@ public class MainMenu {
         return (input.next());
     }
 
+    static String listAdd() { // method that asks for and returns a string, used to get an item to add to the list
+        Scanner input = new Scanner(System.in); // Scanner to get an input
+        System.out.println("What would you like to add to the list?");
+        return(input.next());
+    }
+
     public static void main(String[] args) {
+
+
         boolean exitProg = false;  // bool to control if the program is running
+
+        ArrayList<String> list = new ArrayList<>();
+
         do{ // do loop that returns the user to the menu until they chose to exit the program
             String menuChoice = menu();
             switch (menuChoice) {
-                case "1" -> System.out.println("option 1");
+                case "1" -> list.add(listAdd()); // uses the listAdd method to get an item to add to the list and uses the ArrayList Add() method to add the item to the list
                 case "2" -> System.out.println("option 2");
                 case "3" -> System.out.println("option 3");
                 case "4" -> {
